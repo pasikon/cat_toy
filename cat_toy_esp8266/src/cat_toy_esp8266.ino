a5a822e322ae2479336ca83852f783ba149bda23
michalz <michal.zurek7@gmail.com>
2017-06-22 21:58:59 +0200

    atom proj

---
+++ cat_toy_esp8266/src/cat_toy_esp8266.ino
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+WiFiServer server(1337);
+
+void printWiFiStatus();
+
+void setup(void) {
+  Serial.begin(9600);
+
+  // Configure GPIO2 as OUTPUT.
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  // Start TCP server.
+  server.begin();
+}
+
+void loop(void) {
+
+  WiFiClient client = server.available();
+
+  if (client) {
+    Serial.println("Client connected.");
+
+    while (client.connected()) {
+      if (client.available()) {
+        char command = client.read();
+        if (command == 'H') {
+          Serial.println("manual");
+          Serial.println("left");
+        }
+        else if (command == 'L') {
+          // digitalWrite(ledPin, LOW);
+          Serial.println("LED is now off.");
+        }
+      }
+    }
+    Serial.println("Client disconnected.");
+    client.stop();
+  }
+}
+
+void printWiFiStatus() {
+  Serial.println("");
+  Serial.print("Connected to ");
+  // Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
